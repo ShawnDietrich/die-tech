dd424ad9ee522d72639fd2d87e9559363cd596ac
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-07-23 21:05:52 -0400

    removed date and time from booking page

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -16,12 +16,9 @@ const Book = () => {
                 Accept: "application/json",
             },
             body: JSON.stringify({
-                Date: e.target[0].value,
-                Time: e.target[1].value,
-                FirstName: e.target[2].value,
-                LastName: e.target[3].value,
-                Email: e.target[4].value,
-                Notes: e.target[5].value
+                Name: e.target[0].value,
+                Email: e.target[1].value,
+                Notes: e.target[2].value
             })
         }).then(() => {
             setShowMesg(true)
@@ -32,7 +29,7 @@ const Book = () => {
     return (
         <>
             <div className="bookHeader">
-                <h1>Fill Out The Form Below For A Free Consultation</h1>
+                <h1></h1>
             </div>
             <div className="formBackground">
                 <div className="bookForm">
@@ -40,26 +37,10 @@ const Book = () => {
 
                     <Form id="bookingForm" className="form" validated={validate} onSubmit={handleSubmit}>
 
-                        <Row className="mb-3">
-                            <Form.Group as={Col} controlId='formGridDate'>
-                                <Form.Label>Requested Date Of Consultation</Form.Label>
-                                <FormControl name="date" type="date" required />
-                            </Form.Group>
-                            <Form.Group as={Col} className="mp-3">
-                                <Form.Label>Requested Time Of Day</Form.Label>
-                                <FormControl name="time" type="time" required />
-                            </Form.Group>
-                        </Row>
-
                         <Row className='mb-3'>
                             <Form.Group as={Col} controlId='formGridFirstName'>
-                                <Form.Label>First Name</Form.Label>
+                                <Form.Label>Name</Form.Label>
                                 <FormControl name="firstName" type='firstName' placeholder='Enter Name' required />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId='formGridLastName'>
-                                <Form.Label>Last Name</Form.Label>
-                                <FormControl name="lastName" type='lastName' placeholder='Enter Last Name' required />
                             </Form.Group>
                         </Row>
 
@@ -72,7 +53,7 @@ const Book = () => {
 
                         <Row className="mb-3">
                             <Form.Group className="mb-3" controlId="ControlTextarea">
-                                <Form.Label>Notes:</Form.Label>
+                                <Form.Label>Any additional information:</Form.Label>
                                 <Form.Control as="textarea" rows={3} />
                             </Form.Group>
                         </Row>
