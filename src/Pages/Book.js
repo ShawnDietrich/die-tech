ffdb04b62418663abdf5412792186709d25654cb
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-07-23 21:09:25 -0400

    added heading div

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -28,6 +28,9 @@ const Book = () => {
 
     return (
         <>
+            <div className="bookHeader">
+                
+            </div>
             <div className="formBackground">
                 <div className="bookForm">
                     <Alert key='info' variant="info" show={showMesg}>Your Message Has Been Sent</Alert>
